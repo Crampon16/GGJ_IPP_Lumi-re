d4a3c6103f0c076e86ddd750cc418f0bbcd3e975
Liam Rampon <LiamRampon@MacBook-Pro-de-Liam-2.local>
2021-01-31 13:09:32 +0100

    FIXED: crystal stopping beams, Galileo interacting range

--- ggj_ipp_lumiere/Assets/Scripts/LightSource.cs
+++ ggj_ipp_lumiere/Assets/Scripts/LightSource.cs
@@ -39,9 +39,10 @@ public class LightSource : MonoBehaviour
 	//Call this first because beams won't be destroyed until end of frame
 	public void PrepareRecomputePath()
 	{
+		if (!update)
+			for(int i = 0; i < beams.Count; ++i)
+				Destroy(beams[i]);
 		update = true;
-		for(int i = 0; i < beams.Count; ++i)
-			Destroy(beams[i]);
 	}
 
 	void RecomputePath()
@@ -95,7 +96,7 @@ public class LightSource : MonoBehaviour
 				
 				//Resize to look like a beam
 				float length_tf = (light_path[light_path.Count - 1] - hit.point).magnitude/2;
-				length_tf /= (length_tf + (float)0.5)/length_tf; //black magic, don't touch
+				length_tf /= (length_tf + (float)1.5)/length_tf; //black magic, don't touch
 				beam.transform.localScale += Vector3.up * length_tf;
 				beam.transform.localScale -= ((float)9/10)*(Vector3.right + Vector3.forward);
 
@@ -106,9 +107,9 @@ public class LightSource : MonoBehaviour
 				//Stop the ray if it does not hit a mirror
 				if (hit.collider.gameObject.GetComponent<Mirror>() == null)
 				{
-					if (!isCrystalHit) //But continue if it is a crystal
-						ray_absorbed = true;
-					if(hit.collider.gameObject.GetComponent<RotateMirror>() == null) //or Galileo
+					//But continue if it is a crystal or Galileo
+					if ( (!isCrystalHit)
+					&& hit.collider.gameObject.GetComponent<RotateMirror>() == null) 
 						ray_absorbed = true;
 				}
 				else //Or make it bounce
@@ -123,6 +124,9 @@ public class LightSource : MonoBehaviour
 				ray_absorbed = true;
 				light_path.Add(light_path[light_path.Count - 1] + dir * 100);
 			}
+
+			if(beams.Count > 10)
+				Debug.Log("Infinite loop?");
 		}
 
 		Debug.Log("Number of bounces: " + (light_path.Count - 1));
